93b63124dc3c7b747711f5627b38c63b1ced15c8
Victoria Fields <victoriaashleyfields@gmail.com>
2023-02-23 14:31:00 -0700

    Create file structure for Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <h1>I am App</h1>
+    <main>
+      <h1>I am App</h1>
+      <Header />
+    </main>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <h1>I am Header</h1>
+  );
+}
+
+export default Header;
