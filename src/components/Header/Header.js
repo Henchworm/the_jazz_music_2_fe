1cf0461b173cd2b314d34e15f4b024c333f30ed7
Victoria Fields <victoriaashleyfields@gmail.com>
2023-02-23 15:12:00 -0700

    Add icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,10 @@
 import './Header.css';
 
+const github = require('../../assets/github.png');
+const insta = require('../../assets/instagram.png');
+const twitter = require('../../assets/twitter.png');
+const youtube = require('../../assets/youtube.png');
+
 function Header() {
   return (
     <nav className="main-navigation">
@@ -9,6 +14,12 @@ function Header() {
       </div>
       <div className="links-container">
         <p>Home About Live Music Blarg</p>
+        <div className="icons">
+          <img src={github} />
+          <img src={insta} />
+          <img src={twitter} />
+          <img src={youtube} />
+        </div>
       </div>
     </nav>
   );
