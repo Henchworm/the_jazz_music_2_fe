5a21984fd737c8d28a571a212ca64f396f8267e3
Victoria Fields <victoriaashleyfields@gmail.com>
2023-02-28 16:17:11 -0700

    Add pic to About section and make responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,12 @@
 import './About.css';
 
-const insta = require('../../assets/instagram.png');
+const chrisabout = require('../../assets/chrisabout.png');
 
 function About() {
   return (
     <section className="about-section">
       <div className="about-container">
-        <img src={insta} alt="A description of a future picture of Chris!" />
+        <img src={chrisabout} alt="Chris smiling while playing the gamelan" />
         <div className="about-text">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
         </div>
