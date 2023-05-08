755ffd630e5b7208355ced24763017690048d3ca
Victoria Fields <victoriaashleyfields@gmail.com>
2023-05-07 20:15:17 -0600

    Update badurl alt text

--- src/components/BadUrl/BadUrl.js
+++ src/components/BadUrl/BadUrl.js
@@ -6,7 +6,7 @@ function BadUrl() {
   return (
     <section className='badurl-section'>
       <h1>Oops, that's not a valid URL!</h1>
-      <img src={chrisPet} />
+      <img src={chrisPet} alt="Chris's pet cat looks at you while sitting in a pair of pants."/>
       <p>Try going <a href="/">home</a> or entering another address!</p>
     </section>
   );
