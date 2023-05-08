a12e889d216ff7ec099b4db42b47a57a5119dc86
Victoria Fields <victoriaashleyfields@gmail.com>
2023-05-07 20:09:12 -0600

    Add badUrl stlye

--- src/components/BadUrl/BadUrl.js
+++ src/components/BadUrl/BadUrl.js
@@ -1,9 +1,13 @@
 import './BadUrl.css';
 
+const chrisPet = require('../../assets/badurl.png');
+
 function BadUrl() {
   return (
-    <section>
-      <h1>I am BadUrl</h1>
+    <section className='badurl-section'>
+      <h1>Oops, that's not a valid URL!</h1>
+      <img src={chrisPet} />
+      <p>Try going <a href="/">home</a> or entering another address!</p>
     </section>
   );
 }
