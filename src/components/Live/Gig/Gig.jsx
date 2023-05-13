832266505ee9974682e89a79ba0e87be3e3f9186
Victoria Fields <victoriaashleyfields@gmail.com>
2023-05-13 13:32:56 -0600

    Make live page responsive

--- src/components/Live/Gig/Gig.jsx
+++ src/components/Live/Gig/Gig.jsx
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { getDisplayDate, getDisplayDay, getStandardDisplayTime } from '../../../helpers';
 import './Gig.css';
 
 function Gig({band, date, details, link, venue}) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isWide, setIsWide] = useState(windowWidth >= 790);
   const [isExpanded, setIsExpanded] = useState(false);
+  
+  useEffect(() => {
+    const updateWidth = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', updateWidth);
+    if (windowWidth >= 790) {
+      setIsWide(true);
+    } else {
+      setIsWide(false);
+    }
+        
+    return(() => {
+        window.removeEventListener('resize', updateWidth);
+    })
+  }, [windowWidth]);
 
   let infoBtnText = 'Show Details';
   if (isExpanded) {
@@ -19,28 +35,61 @@ function Gig({band, date, details, link, venue}) {
     window.open(url, '_blank');
   }
 
+  const getResponsiveDate = (date) => {
+    const displayDay = getDisplayDay(date);
+    const displayDate = getDisplayDate(date).split(',')[0];
+    let responsiveDay;
+    if (displayDay.toLowerCase().includes('tues') || displayDay.toLowerCase().includes('thurs')) {
+      responsiveDay = displayDay.toLowerCase().includes('tues') ? 'Tues' : 'Thurs';
+    } else {
+      responsiveDay = displayDay.substring(0, 3);
+    }
+    const responsiveDate = `${responsiveDay} ${displayDate}`;
+    return responsiveDate;
+  }
+
   return (
-    <article className='gig-card'>
-        <div className='info-container'>
-          <div className='date-container'>
+    <>
+    {isWide && <article className='wide-gig-card'>
+        <div className='wide-info-container'>
+          <div className='wide-date-container'>
             <span><b>{getDisplayDate(date)}</b></span>
             <br />
             <span>{getDisplayDay(date)}</span>
           </div>
-          <div className='venue-container'>
+          <div className='wide-venue-container'>
             <span><b>{band}</b></span>
             <br />
             <span>{`${venue} @ ${getStandardDisplayTime(date)}`}</span>
           </div>
-          <div className='button-container'>
+          <div className='wide-button-container'>
             <button onClick={handleInfoClick}>{infoBtnText}</button>
             <button onClick={handleTicketClick}>More Info</button>
           </div>
         </div>
-        {isExpanded && <div className='more-info'>
+        {isExpanded && <div className='wide-details-container'>
           <p>{details}</p>
         </div>}
-      </article>
+      </article>}
+      {!isWide && <article className='gig-card'>
+        <div className='info-container'>
+          <div className='date-venue-container'>
+            <p className='responsive-date'>{getResponsiveDate(date)}</p>
+            <p className='band'>{band}</p>
+            <span>{`${venue}`}</span>
+            <br />
+            <span>@ {`${getStandardDisplayTime(date)}`}</span>
+          </div>
+          <div className='button-container'>
+            <button onClick={handleTicketClick}>More Info</button>
+            <button onClick={handleInfoClick}>{infoBtnText}</button>
+          </div>
+        </div>
+        {isExpanded && <div className='details-container'>
+          <p className='details'>{details}</p>
+        </div>}
+      </article>}
+    </>
   )
 }
 
